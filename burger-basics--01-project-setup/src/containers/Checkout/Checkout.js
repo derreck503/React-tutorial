9eda08d8f0791da2a3e20af5da67a78dd14977db
Derreck Stellpflug <derreck@Derrecks-iMac.local>
2018-04-05 22:45:20 -0500

    minor fix

--- burger-basics--01-project-setup/src/containers/Checkout/Checkout.js
+++ burger-basics--01-project-setup/src/containers/Checkout/Checkout.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
+import * as actions from '../../store/actions/index';
 
 class Checkout extends Component {
 
@@ -18,6 +19,7 @@ class Checkout extends Component {
     render () {
         let summary = <Redirect to="/" />
         if (this.props.ings) {
+            const purchasedRedirect = this.props.purchased ? <Redirect to="/" /> : null;
             summary = (
                 <div>
                     <CheckoutSummary
@@ -40,8 +42,9 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.burgerBuilder.ingredients
-    }
+        ings: state.burgerBuilder.ingredients,
+        purchased: state.order.purchased
+    };
 };
 
 export default connect(mapStateToProps)(Checkout);
